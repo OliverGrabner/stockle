f9ebf9773ce44f89b82d4053da89ff40aa21ce8b
Oliver Grabner <oliver.charles.rene.grabner@gmail.com>
2026-01-15 13:44:11 -0600

    updated java version

--- k8s/build.sh
+++ k8s/build.sh
@@ -2,8 +2,8 @@
 
 # STOCKLE GAME HOSTING
 
-sudo docker build -t localhost:32000/stockle-backend:2026-1-15-a .
-sudo docker push localhost:32000/stockle-backend:2026-1-15-a
+sudo docker build -t localhost:32000/stockle-backend:2026-1-15-c .
+sudo docker push localhost:32000/stockle-backend:2026-1-15-c
 
 sudo docker build -t localhost:32000/stockle-data-fetcher:2026-1-1-a .
 sudo docker push localhost:32000/stockle-data-fetcher:2026-1-1-a
